5265d465ecfb83348a14e9b94bc46b0ea6a2885d
CharanMN7_pc <maverickid247@gmail.com>
2023-10-25 23:27:35 +0530

    Separated the nothing-to-see-here screen as a component

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { NothingToSeeHere } from "./screens/NothingToSeeHere";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <figure>
-    <img
-      src="./src/assets/images/undraw_tree_swing_re_pqee.svg"
-      alt="google developer student clubs raghu engineering college logo"
-    />
-    <figcaption>Oops! Nothing to see here 😅</figcaption>
-    <p>Anyway, now that you're here, why not check out our club?</p>
-    <a href="https://gdsc.community.dev/raghu-engineering-college-visakhapatnam/">
-      <span>Explore GDSC REC</span>
-      <img
-        src="./src/assets/images/google-developers-seeklogo.com.svg"
-        alt="google developer student clubs raghu engineering college logo"
-      />
-    </a>
-  </figure>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<NothingToSeeHere />);
