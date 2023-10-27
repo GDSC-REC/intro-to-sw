84af0b8d149c494bb53f9a225b18fa80b81d0e4b
CharanMN7_pc <maverickid247@gmail.com>
2023-10-27 16:31:30 +0530

    Added Hero & Footer to Home

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,6 +1,13 @@
 import "./Home.css";
 import { Link } from "react-router-dom";
+import { Hero } from "./home-components/Hero";
+import { Footer } from "./reusable-components/Footer";
 
 export const Home = () => {
-  return <div></div>;
+  return (
+    <>
+      <Hero />
+      <Footer />
+    </>
+  );
 };
