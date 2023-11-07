83a3bbd2568621ee0608906150f119051d2e4bad
CharanMN7_pc <maverickid247@gmail.com>
2023-11-07 14:52:59 +0530

    Added some stickers to the curriculum page

--- src/screens/curriculum-components/Day.jsx
+++ src/screens/curriculum-components/Day.jsx
@@ -1,21 +1,25 @@
 import data from "./data.json";
 import "./Day.css";
+import gdsccolorlogo from "../../assets/stickers/Google_for_Developers_logomark_color.png";
 
 export const Day = ({ dayNum, title, content }) => {
   return (
     <div className="day">
-      <h3>day {dayNum}</h3>
-      <p>
-        <span>{title}</span> <br />
-        {content.map((line, i) => {
-          return (
-            <>
-              <span key={i}>{line}</span>
-              <br />
-            </>
-          );
-        })}
-      </p>
+      <div>
+        <h3>day {dayNum}</h3>
+        <p>
+          <span>{title}</span> <br />
+          {content.map((line, i) => {
+            return (
+              <>
+                <span key={i}>{line}</span>
+                <br />
+              </>
+            );
+          })}
+        </p>
+      </div>
+      <img src={gdsccolorlogo} alt="" />
     </div>
   );
 };
