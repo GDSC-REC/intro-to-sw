b8516fc0d8b889376cb5149311b32f471e3af111
CharanMN7_pc <maverickid247@gmail.com>
2023-11-07 13:39:10 +0530

    Added the Days to the Curriculum

--- src/screens/Curriculum.jsx
+++ src/screens/Curriculum.jsx
@@ -1,10 +1,18 @@
 import "./Curriculum.css";
 import { Link } from "react-router-dom";
+import data from "./curriculum-components/data.json";
+import { Day } from "./curriculum-components/Day";
 
 export const Curriculum = () => {
   return (
-    <div>
-      <h1>Curriculum</h1>
-    </div>
+    <section id="curr">
+      <h2>Curriculum</h2>
+      <div>
+        {console.log(data)}
+        {data.data.map((item) => {
+          return <Day dayNum={item.dayNum} title={item.title} content={item.content} />;
+        })}
+      </div>
+    </section>
   );
 };
---
+++ src/screens/curriculum-components/Day.jsx
@@ -0,0 +1,22 @@
+import data from "./data.json";
+import "./Day.css";
+
+export const Day = ({ dayNum, title, content }) => {
+  return (
+    <div className="day">
+      <h3>day {dayNum}</h3>
+      <p>
+        <span>{title}</span> <br />
+        {console.log(content)}
+        {content.map((line, i) => {
+          return (
+            <>
+              <span key={i}>{line}</span>
+              <br />
+            </>
+          );
+        })}
+      </p>
+    </div>
+  );
+};
