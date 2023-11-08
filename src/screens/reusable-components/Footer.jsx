58c69d1111f057a539814fd30e7247ad2552a19b
CharanMN7_pc <maverickid247@gmail.com>
2023-11-09 02:35:59 +0530

    Generalized branding in footer

--- src/screens/reusable-components/Footer.jsx
+++ src/screens/reusable-components/Footer.jsx
@@ -3,28 +3,14 @@ import "./Footer.css";
 export const Footer = () => {
   return (
     <footer>
-      <p>
-        <a href="" className="footlink">
-          GDSC REC
-        </a>{" "}
-        x{" "}
-        <a href="" className="footlink">
-          GDSC UI
-        </a>
-      </p>
+      <p style={{ color: "white" }}>devsForFun(❤️)</p>
       <p>
         website design inspired by{" "}
         <a href="https://buildspace.so/" className="footlink">
           buildspace
         </a>
-        ❤️
-        <br />
-        by{" "}
-        <a href="https://linktr.ee/charan_manikanta" className="footlink">
-          Charan, GDSC REC
-        </a>
       </p>
-      <p>this program is not endorsed by Google or the GDSC program in any way</p>
+      <p>this program is not endorsed by Google, GDSC program, or buildspace in any way</p>
     </footer>
   );
 };
